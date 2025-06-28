8d7cb2cf9b1bfa2d58c710d804daeb105e6de2a2
Matías Loyola <matiazloyola@gmail.com>
2025-06-28 02:37:49 -0400

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBdaBDqlmHbGE0YXpPUslSmzkiAFhNbuYE",
-  authDomain: "veranum-d93ce.firebaseapp.com",
-  projectId: "veranum-d93ce",
-  storageBucket: "veranum-d93ce.appspot.com",
-  messagingSenderId: "1058689801599",
-  appId: "TU_APP_ID"
-};
-firebase.initializeApp(firebaseConfig);
