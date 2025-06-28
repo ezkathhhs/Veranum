566e1e1ad421f2209f40bf75e4de34a1b5daaac4
ezkathhhs <matiazloyola@gmail.com>
2025-06-27 23:26:44 -0400

    Funcionalidades Login, register

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBdaBDqlmHbGE0YXpPUslSmzkiAFhNbuYE",
+  authDomain: "veranum-d93ce.firebaseapp.com",
+  projectId: "veranum-d93ce",
+  storageBucket: "veranum-d93ce.appspot.com",
+  messagingSenderId: "1058689801599",
+  appId: "TU_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
